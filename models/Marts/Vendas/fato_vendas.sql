efd258a14ebfd5083a8380303172da8f24e74663
fernandopikler@gmail.com <fernandopikler@gmail.com>
2023-01-27 17:52:47 +0000

    adicionar stage de sales order reason

--- models/Marts/Vendas/fato_vendas.sql
+++ models/Marts/Vendas/fato_vendas.sql
@@ -11,27 +11,18 @@ SELECT
 from
 {{ ref('stg_salesorderdetail') }}
 )
-,
-territorio as (
-SELECT 
-    *
-from
-{{ ref('stg_salesterritory') }}
-)
 
 select
-    a.pk_pedidovenda
-    ,b.pk_detalhevenda
+    b.pk_detalhevenda
+    ,a.pk_pedidovenda
+    ,b.fk_produto
     ,b.preco_unitario
     ,b.quantidade_pedido
     ,a.data_pedido
     ,a.sk_ordemcompra
     ,a.sk_cartaocredito
     ,a.sk_vendedor
-    ,c.nome_territorio
-    ,c.sk_paisregiao
     ,a.sk_endcobranca
     ,a.sk_cliente
 from fato_vendas a
 left join detalhes_vendas b on a.pk_pedidovenda = b.fk_pedidovenda
-left join territorio c on a.sk_territorio = c.pk_territoriovendas
