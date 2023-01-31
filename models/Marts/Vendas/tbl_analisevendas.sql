45bbd6ad0dcc8e1588ceb729c93d7fe9fa79a460
fernandopikler@gmail.com <fernandopikler@gmail.com>
2023-01-31 20:26:05 +0000

    criação da tabela de análise para BI

--- models/Marts/Vendas/tbl_analisevendas.sql
+++ models/Marts/Vendas/tbl_analisevendas.sql
@@ -40,4 +40,26 @@ SELECT
 FROM  {{ ref('dim_vendedores') }} 
 )
 
-select * from fato_vendas
+select 
+    a.idvenda
+    ,a.idpedido
+    ,a.data_pedido as data_venda
+    ,a.status
+    ,a.preco_un
+    ,a.soma_quantidade
+    ,b.nome_produto
+    ,c.tipo_cartaocredito
+    ,d.nome_completo as nome_cliente
+    ,e.endereco_linha1 as endereco
+    ,e.cidade_endereco
+    ,e.nome_estadoprovincia as estado_endereco
+    ,e.nome_pais as pais_endereco
+    ,f.nome_razao_venda
+    ,h.nome_completo as nome_completo_vendedor
+from fato_vendas a
+left join dim_produtos b on a.fk_produto = b.pk_produto
+left join dim_cartaocredito c on a.fk_cartaocredito = c.pk_cartaocredito
+left join dim_clientevendas d on a.fk_cliente = d.pk_customer
+left join dim_enderecovenda e on a.fk_endcobranca = e.pk_endereco
+left join dim_razaovenda f on a.fk_razaovenda = f.pk_razaovenda
+left join dim_vendedores h on a.fk_vendedor = h.pk_entidadebusiness
